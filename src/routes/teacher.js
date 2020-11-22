8825e65fb2b343ddc7e82a0e249156bc18ec5b50
david ballesteros <david.ballesteros@udea.edu.co>
2020-11-21 22:00:04 -0500

    Practicas teacher

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -19,20 +19,25 @@ router.get('/reserve/list/zoomin/:id', async (req, res) => {
     const reserve = await pool.query('SELECT * FROM RESERVEG WHERE ID=?', [id]);
     const practice = await pool.query('SELECT * FROM PRACTICE p WHERE p.id = ?', [reserve[0].id_practice]);
     const devices = await pool.query('SELECT gd.name as name, gt.name as type, gd.amount as amount, gd.ports as ports FROM RESERVEG r JOIN RESERVEBYDEVICE rd ON r.id = rd.reserve JOIN SPECIFICDEVICE sd ON rd.device = sd.id JOIN GENERALDEVICE gd ON sd.type = gd.id JOIN GENERALTYPE gt ON gd.type = gt.id WHERE r.id = ?', [id]);
-    res.render('teacher/zoominPracticesList', { practice: practice[0], devices });
+    res.render('teacher/zoominReservesList', { practice: practice[0], devices });
 });
 
 router.get('/reserve/list/edit/:id', async (req, res) => {
     const { id } = req.params;
     console.log(id);
-    const reserve = await pool.query('SELECT r.id as id, r.course as course, r.day as day, r.startHour as startHour, r.endHour as endHour, r.podsAmount as podsAmount, p.name as name FROM RESERVEG r JOIN PRACTICE p ON r.id_practice = p.id WHERE r.id = ?', [id]);
+    var reserve = await pool.query('SELECT r.id as id, r.course as course, r.day as day, r.startHour as startHour, r.endHour as endHour, r.podsAmount as podsAmount, p.name as name FROM RESERVEG r JOIN PRACTICE p ON r.id_practice = p.id WHERE r.id = ?', [id]);
     const practices = await pool.query('SELECT * FROM PRACTICE');//Le falta filtro a la practica
-    console.log(reserve[0]);
+    const d = reserve[0].day;
+    const fecha = new Date(d);
+    console.log(fecha);
+    reserve[0].day = fecha.getUTCFullYear() + '-' + fecha.getUTCMonth() + '-' + fecha.getUTCDate();
+    console.log(reserve[0].day);
     res.render('teacher/editReserve', { reserve: reserve[0], practices });
 });
 router.post('/reserve/list/edit/:id', async (req, res) => {
     const { id } = req.params;
     const { name, day, startHour, endHour, course, podsAmount } = req.body;
+    console.log(req.body);
     const newReserve = {
         id_practice: name,
         day,
@@ -59,15 +64,6 @@ router.get('/reserve/create', async (req, res) => {
 });
 router.post('/reserve/create', async (req, res) => {
     const { name, day, startHour, endHour, course, podsAmount } = req.body;
-    console.log(name);
-    const newReserve = {
-        id_practice: name,
-        day,
-        startHour,
-        endHour,
-        course: 'COM2',
-        podsAmount
-    };
     await pool.query('INSERT INTO RESERVEG(id_practice, day, startHour, endHour, course, podsAmount) values(?,?,?,?,?,?)', [name, day, startHour, endHour, 'COM2', podsAmount]);
     req.flash('success', 'PRÁCTICA CREADA EXITOSAMENTE');
     res.redirect('/teacher/reserve/list');
@@ -75,8 +71,7 @@ router.post('/reserve/create', async (req, res) => {
 
 router.get('/practice/list', async (req, res) => {
     //Un profesor tiene unas practicas, por lo cual falta unir eso en el modelo entidad relacion.
-    var practices = await pool.query('SELECT * FROM PRACTICE');
-    //console.log(practices);
+    var practices = await pool.query('SELECT p.id ,p.name, p.pods, COUNT(dp.practice) as cantidad FROM PRACTICE p LEFT JOIN DEVICEBYPRACTICE dp ON p.id = dp.practice GROUP BY p.id ,p.name, p.pods');
     Object.keys(practices).forEach(practice => {
         if(practices[practice].pods == true){
             practices[practice].pods='SI';
@@ -103,7 +98,6 @@ router.post('/practice/add/next', async (req, res) => {
     if (practice[0] == undefined) {
         await pool.query('INSERT INTO practice(name, description,pods)VALUES(?,?,?)', [req.body.name, req.body.description, pod]);
         practice = await pool.query('SELECT * FROM PRACTICE WHERE name = ? and description = ? and pods = ?', [req.body.name, req.body.description, pod]);
-        console.log(practice[0]);
         const { name } = req.body;
         const types = await pool.query('SELECT * FROM GENERALTYPE');
         res.render('teacher/nextCreatePractice', { name, types, practice: practice[0] });
@@ -121,17 +115,51 @@ router.get('/practice/add/next', (req, res) => {
 });
 
 router.post('/practice/add/next/confirm', (req, res) => {
-    res.redirect('/teacher/reserve/list');
+    res.redirect('/teacher/practice/list');
 });
 
-router.get('/practice/add/cancel/:practice', async (req, res) => {
+router.get('/practice/add/next/cancel/:practice', async (req, res) => {
     const { practice } = req.params;
-    console.log('cancelando');
     await pool.query('DELETE FROM PRACTICE WHERE id = ?',[practice]).catch(err => {
         console.log(err);
         res.send('No se encuentra la práctica');
     });
-    res.redirect('/teacher/reserve/list');
+    res.redirect('/teacher/practice/list');
+});
+
+router.get('/practice/list/zoomin/:id', async (req, res) => {
+    const { id } = req.params;
+    const practice = await pool.query('SELECT * FROM PRACTICE p WHERE p.id = ?', [id]);
+    const devices = await pool.query('SELECT gd.id as id,gd.name as name, gt.name as type, gd.ports as ports FROM PRACTICE p JOIN DEVICEBYPRACTICE dp ON p.id = dp.practice JOIN GENERALDEVICE gd ON dp.device = gd.id JOIN GENERALTYPE gt ON gd.type = gt.id WHERE p.id=?', [id]);
+    res.render('teacher/zoominPracticesList', { practice: practice[0], devices });
+});
+
+router.get('/practice/list/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const practice = await pool.query('SELECT * FROM PRACTICE WHERE id = ?', [id]);
+    const types = await pool.query('SELECT * FROM GENERALTYPE');
+    const devices = await pool.query('SELECT gd.id as id,gd.name as name, gt.name as type, gd.ports as ports FROM PRACTICE p JOIN DEVICEBYPRACTICE dp ON p.id = dp.practice JOIN GENERALDEVICE gd ON dp.device = gd.id JOIN GENERALTYPE gt ON gd.type = gt.id WHERE p.id=?', [id]);
+    res.render('teacher/editPractice', { practice: practice[0], types, devices});
+});
+
+router.post('/practice/list/edit/:id', async (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    var { name, description, pods, tipo } = req.body;
+    const { id } = req.params;
+    if(pods == 'on'){
+        pods = true;
+    }else{
+        pods = false;
+    }
+    const newPractice = {
+        name,
+        description,
+        pods
+    }
+    await pool.query('UPDATE PRACTICE set ? WHERE id = ?', [newPractice, id]);
+    req.flash('success', 'PRÁCTICA EDITADA EXITOSAMENTE');
+    res.redirect('/teacher/practice/list');
 });
 
 //Rutas ajax
@@ -154,10 +182,15 @@ router.get('/practice/add/device/:practice/:device', async (req, res) => {
 
 router.get('/practice/add/delete/:practice/:device', async (req, res) => {
     const { practice, device } = req.params;
-    console.log(req.params);
-    const dev = await pool.query('SELECT * FROM DEVICEBYPRACTICE WHERE practice = ? AND device = ?', [practice, device]);
-    console.log(dev);
     await pool.query('DELETE FROM DEVICEBYPRACTICE WHERE practice = ? AND device = ?', [practice, device]);
-    res.json(dev);
+    res.json('');
 });
+
+router.get('/practice/list/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log(req.params);
+    await pool.query('DELETE FROM PRACTICE WHERE id = ?', [id]);
+    res.json('');
+});
+
 module.exports = router;
