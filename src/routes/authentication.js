18d03749d894ae173c537f44408a8ebb66dd2449
jharpx10 <juana.rios@udea.edu.co>
2020-11-23 10:19:59 -0500

    t

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,24 +1,90 @@
 const express = require('express');
 const router = express.Router();
-
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { route } = require('.');
 router.get('/', (req, res) => {
     res.render('auth/signin');
 });
 
-router.post('/login', async (req, res) => {
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+router.post('/signup', (req, res) => {
+    console.log(req.body);
+    res.send("recivido");
+});
+
+router.post('/login', authUser,ensureToken,async (req, res) => {
+
+    if (req.user.role == 1005) {
+
+        res.setHeader('Authorization', 'Bearer ' + user.token);
+        res.redirect('admin/');
+    }
+    
+});
+
+async function authRole(role) {
+    return (res, req, next => {
+        if (req.user.role !== role) {
+            res.send('Not allowed');
+        }
+        next();
+    });
+
+
+}
+
+
+async function authUser(req, res, next) {
     const { username } = req.body;
     const { password } = req.body;
-   
     const axios = require('axios')
 
-    const a=await axios.post('https://lis.udea.edu.co/api/ldap/login', {
-            'username': username,
+    const a = await axios.post('https://lis.udea.edu.co/api/ldap/login', {
+        'username': username,
         'password': password
     }).catch(e => {
         req.flash('danger', '¡USUARIO y/o CONSTRASEÑA INVÁLIDOS!');
+        res.redirect('/auth/');
       
     });
-    console.log(a.data);
-});
 
+    req.token = a.data.token;
+    next();
+}
+
+
+function ensureToken(req, res, next){
+    const bearerheader = req.token;
+  
+    if (typeof bearerheader !== 'undefined') {
+       
+        jwt.verify(bearerheader, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+            if (err) {
+                res.sendStatus(403);
+
+            } else {
+               
+                const user = data.user;
+                const token = bearerheader;
+                const role = data.roleCode;
+                req.user = { user, role, token};  
+               
+                next();
+            }
+
+
+        });
+      
+    } else {
+        res.sendStatus(401);
+       
+    }
+  
+
+}
+module.exports.authRole = authRole;
 module.exports = router;
